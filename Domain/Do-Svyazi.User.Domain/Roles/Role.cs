5f19b69fc150b41f4dbb6a50b25ed87b5698471d
iskander-faggod <kudashev.iskander13@gmail.com>
2022-05-29 21:49:28 +0300

    fix: remove separate lines in Role.cs

--- Domain/Do-Svyazi.User.Domain/Roles/Role.cs
+++ Domain/Do-Svyazi.User.Domain/Roles/Role.cs
@@ -9,18 +9,11 @@ public class Role
     public ActionOption CanDeleteMessage { get; set; }
     public ActionOption CanWriteMessage { get; set; }
     public ActionOption CanReadMessage { get; set; }
-
     public ActionOption CanAddUsers { get; set; }
-
     public ActionOption CanDeleteUsers { get; set; }
-
     public ActionOption CanPinMessages { get; set; }
-
     public ActionOption CanSeeChannelMembers { get; set; }
-
     public ActionOption CanInviteOtherUsers { get; set; }
-
     public ActionOption CanEditChannelDescription { get; set; }
-
     public ActionOption CanDeleteChat { get; set; }
 }
