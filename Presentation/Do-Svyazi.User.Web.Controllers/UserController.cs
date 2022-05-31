1b2f184e8e1c1b1ca32daad3f1100669ed1ab2bf
iskander-faggod <kudashev.iskander13@gmail.com>
2022-05-31 16:01:15 +0300

    feat: add commands to controller

--- Presentation/Do-Svyazi.User.Web.Controllers/UserController.cs
+++ Presentation/Do-Svyazi.User.Web.Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using Do_Svyazi.User.Application.CQRS.Users.Commands;
 using Do_Svyazi.User.Application.CQRS.Users.Queries;
+using Do_Svyazi.User.Domain.Users;
 using Do_Svyazi.User.Dtos.Users;
 using MediatR;
 using Microsoft.AspNetCore.Http;
@@ -23,11 +24,35 @@ public class UserController : ControllerBase
         return Ok(response.users);
     }
 
+    [HttpGet("GetUser")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<MessengerUser>> GetUser(Guid userId)
+    {
+        var response = await _mediator.Send(new GetUser.Command(userId));
+        return Ok(response);
+    }
+
     [HttpPost("ChangeNickName")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult> SetNickNameById(Guid userId, string nickName)
     {
         await _mediator.Send(new SetUserNickNameById.Command(userId, nickName));
+        return Ok();
+    }
+
+    [HttpPost("DeleteUser")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult> DeleteUser(Guid userId)
+    {
+        await _mediator.Send(new DeleteUser.Command(userId));
+        return Ok();
+    }
+
+    [HttpPost("AddChatToUser")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult> AddChatToUser(Guid userId, Guid chatId)
+    {
+        await _mediator.Send(new AddChatToUser.Command(userId, chatId));
         return Ok();
     }
 
@@ -37,5 +62,21 @@ public class UserController : ControllerBase
     {
         var response = await _mediator.Send(new AddUser.Command(name, nickName, description));
         return Ok(response);
+    }
+
+    [HttpPost("ChangeDescription")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult> ChangeDescription(Guid userId,  string description)
+    {
+        await _mediator.Send(new ChangeUserDescriptionById.Command(userId, description));
+        return Ok();
+    }
+
+    [HttpPost("ChangName")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult> ChangeName(Guid userId,  string name)
+    {
+        await _mediator.Send(new ChangeUserNameById.Command(userId, name));
+        return Ok();
     }
 }
