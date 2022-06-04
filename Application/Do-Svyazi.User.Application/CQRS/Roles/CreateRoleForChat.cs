6feee284f85fe9672ca1d7a92c66d26a8c65320e
iskander-faggod <kudashev.iskander13@gmail.com>
2022-06-04 15:07:18 +0300

    feat: add in controller

--- Application/Do-Svyazi.User.Application/CQRS/Roles/CreateRoleForChat.cs
+++ Application/Do-Svyazi.User.Application/CQRS/Roles/CreateRoleForChat.cs
@@ -1,20 +1,27 @@
+using AutoMapper;
 using Do_Svyazi.User.Application.DbContexts;
 using Do_Svyazi.User.Domain.Chats;
 using Do_Svyazi.User.Domain.Exceptions;
 using Do_Svyazi.User.Domain.Roles;
+using Do_Svyazi.User.Dtos.Roles;
 using MediatR;
 
 namespace Do_Svyazi.User.Application.CQRS.Roles;
 
 public static class CreateRoleForChat
 {
-    public record Command(Role role, Guid chatId) : IRequest<Unit>;
+    public record Command(RoleDto role, Guid chatId) : IRequest<Unit>;
 
     public class Handler : IRequestHandler<Command, Unit>
     {
         private readonly IDbContext _context;
+        private readonly IMapper _mapper;
 
-        public Handler(IDbContext context) => _context = context;
+        public Handler(IDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
 
         public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
         {
@@ -25,7 +32,8 @@ public static class CreateRoleForChat
                     $"Chat with id = {request.chatId} is not created");
             }
 
-            chat.AddRole(request.role);
+            Role? newRole = _mapper.Map<Role>(request.role);
+            chat.AddRole(newRole);
             await _context.SaveChangesAsync(cancellationToken);
             return Unit.Value;
         }
---
+++ Presentation/Do-Svyazi.User.Web.Controllers/RoleController.cs
@@ -0,0 +1,29 @@
+using Do_Svyazi.User.Application.CQRS.Chats.Commands;
+using Do_Svyazi.User.Application.CQRS.Chats.Queries;
+using Do_Svyazi.User.Application.CQRS.Roles;
+using Do_Svyazi.User.Domain.Roles;
+using Do_Svyazi.User.Domain.Users;
+using Do_Svyazi.User.Dtos.Chats;
+using Do_Svyazi.User.Dtos.Roles;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Do_Svyazi.User.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoleController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public RoleController(IMediator mediator) => _mediator = mediator;
+
+    [HttpPost(nameof(CreateRoleForChat))]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult> CreateRoleForChat(RoleDto role, Guid chatId)
+    {
+        await _mediator.Send(new CreateRoleForChat.Command(role, chatId));
+        return Ok();
+    }
+}
